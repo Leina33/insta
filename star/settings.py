a4d3d41d45745d14f2f8e664b333d48a4f24ce46
Leina33 <dominickrutto@gmail.com>
2020-01-10 09:25:53 +0300

    add the whitenoise

--- star/settings.py
+++ star/settings.py
@@ -138,4 +138,4 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
